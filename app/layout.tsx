eca127838174821a2d48650386444121a12cdc3c
ljmyung <ljmyung1224@gmail.com>
2026-08-21 01:46:40 +0900

    Add AdSense verification script

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,13 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="ko"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7896176132444930"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
